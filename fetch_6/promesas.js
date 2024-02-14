fcbb600f2c995d12235313b15ccb0d2fb799f7e2
AldairCruz7 <aldairtusero1@gmail.com>
2024-02-14 11:36:02 -0600

    rama 6 fetch

--- fetch_6/promesas.js
+++ fetch_6/promesas.js
@@ -2,6 +2,7 @@ const sumarLento = (numero) => {
     return new Promise((resolve, reject) => {
         setTimeout(() => {
             resolve(numero + 1);
+            // reject('Sumar lento fallo');
         }, 800);
     });
 }
@@ -15,9 +16,9 @@ const sumarRapido = (numero) => {
 }
 
 // Promise.race compite entre las promesas y devuelve la primera que se resuelve
-Promise.race([sumarLento(5), sumarRapido(10)]) // esta promesa se resuelve con la promesa sumarRapido
+// Si falla una, fallan todas
+Promise.race([sumarLento(5), sumarRapido(10)])
     .then(respuestas => {
         console.log(respuestas);
-        
     })
     .catch(console.log);
